93a15c37234f37fe1cade7c094b7c25722d35d00
gentlemanjs <jungsik321@gmail.com>
2022-05-02 09:49:56 +0900

    2022.05.02

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+import Posts from "../../components/posts/Posts";
+
+export default function Home() {
+    return (
+        <>
+            <main id="main">
+                <Posts />
+            </main>
+        </>
+    );
+}
